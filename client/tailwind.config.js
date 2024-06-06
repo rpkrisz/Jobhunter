e863f91d60464a4541d2a4bb00c95513ee2222d9
Réthey-Prikkel <rp.krisz123@gmail.com>
2024-06-06 04:20:18 +0200

    Add daisyUI

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,4 +1,5 @@
 
+import daisyui from "daisyui"
 
 /** @type {import('tailwindcss').Config} */
 export default {
@@ -8,6 +9,8 @@ export default {
   ], theme: {
     extend: {},
   },
-  plugins: [],
+  plugins: [
+    daisyui,
+  ],
 }
 
