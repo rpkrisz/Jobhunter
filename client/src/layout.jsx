54cde09ac1cc5fa2000b1bccc95f4a48e29ab1d3
Réthey-Prikkel <rp.krisz123@gmail.com>
2024-06-06 13:59:13 +0200

    Layout changes

--- client/src/layout.jsx
+++ client/src/layout.jsx
@@ -4,17 +4,19 @@ import Menu from "./menu";
 export default function Layout() {
   return (
     <>
-      <header>
+      <header className="header ">
         <Menu />
       </header>
-      <main className="flex flex-row justify-center w-full h-[90dvh] p-6 ">
+      <main className="flex flex-row justify-center w-full h-[87dvh] p-6 ">
         <Outlet />
       </main>
-      <footer className="flex justify-center h-auto">
-        <p>
-          © 2024 Réthey-Prikkel Kriszsztián - Elte IK Progremtervező informatika
-          - Kliensoldali webprogramozás beadandó
-        </p>
+      <footer className="footer footer-center p-4 bg-base-300 text-base-content">
+        <aside>
+          <p>
+            Copyright © 2024 Réthey-Prikkel Kriszsztián - Elte IK Progremtervező
+            informatika - Kliensoldali webprogramozás beadandó
+          </p>
+        </aside>
       </footer>
     </>
   );
